597ac314c220980371616d23b22ef1c2e08c1958
GDINaimHakimi <naimhakimi13m2@gmail.com>
2023-04-17 13:14:55 +0800

    header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -12,10 +12,29 @@
     </head>
     <body class="antialiased">
     <header>
-        <nav class="navbar navbar-dark bg-dark">
-        <span class="navbar-brand">
-                <img src="/laravel logo.png" width="80" height="50" alt="">My Website
-        </span>
-        
-        </nav>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <img src="/laravel logo.png"  width="80" height="50" class="d-inline-block align-top" alt="">
+                <p class="navbar-brand" href="#">My Website</p>
+                
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
 </header>
