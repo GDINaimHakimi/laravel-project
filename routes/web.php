ca99cfa58729ce8efaa597ffb7fbeffe6aab1a0b
GDINaimHakimi <naimhakimi13m2@gmail.com>
2023-04-18 12:51:15 +0800

    t

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,11 @@ Route::get('posts',function () {
 Route::get('posts/{post}',function ($slug) {
     
     $path = __DIR__."/../resources/posts/{$slug}.html";
+    if(!file_exists($path)){
+        return redirect('/');
+        abort(404);
+    }
     $post = file_get_contents($path);
     return view('post',['post'=>$post]);
-});
+})->where('post','[A-z_\-]+');
 ?>
